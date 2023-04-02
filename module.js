06adc3d465d77e42f9a6a264a96d279fbd8e61a6
CptLucky27 <90856406+CptLucky27@users.noreply.github.com>
2023-04-02 23:10:13 +0200

    Add files via upload

--- module.js
+++ module.js
@@ -1,34 +1,123 @@
-async function activateDrawing(drawing) {
-  const characterOptions = game.actors.entities.filter(a => a.data.type === "character").map(a => `<option value="${a.id}">${a.name}</option>`).join("");
-  const content = `
-    <div>
-      <label for="character-select">Select a character:</label>
-      <select id="character-select">${characterOptions}</select>
-    </div>
-  `;
-  const buttons = {
-    yes: {
-      icon: '<i class="fas fa-check"></i>',
-      label: 'Select Character',
-      callback: () => {
-        const selectedCharacterId = $('#character-select').val();
-        drawing.data.flags = { 
-          "unite-and-fight": { 
-            "selectedCharacterId": selectedCharacterId 
-          } 
-        };
-        canvas.updateEmbeddedDocuments("Drawing", [drawing.data]);
-      }
-    },
-    no: {
-      icon: '<i class="fas fa-times"></i>',
-      label: 'Cancel'
-    }
-  };
-  new Dialog({
-    title: 'Select Character',
-    content: content,
-    buttons: buttons,
-    default: 'yes'
-  }).render(true);
+// Define the clickable tile
+class CharacterSelectTile extends Drawing {
+  static get layer() {
+    return "drawings";
+  }
+
+  static get shape() {
+    return "rect";
+  }
+
+  static get colors() {
+    return {
+      fill: "#ff0000",
+      stroke: "#000000",
+      alpha: 0.5
+    };
+  }
+
+  static get dimensions() {
+    return {
+      width: 100,
+      height: 100
+    };
+  }
+
+  static get clickLeft() {
+    return (event) => {
+      event.stopPropagation();
+      const folderOptions = game.actors.entities.filter(entity => entity.data.type === "folder").map(entity => ({id: entity.id, name: entity.name}));
+      new CharacterSelectForm(folderOptions).render(true);
+    };
+  }
 }
+
+// Define the character select form
+class CharacterSelectForm extends FormApplication {
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["sheet"],
+      template: "modules/unite-and-fight/templates/character-select-form.html",
+      width: 400,
+      height: 300,
+      resizable: true,
+      title: "Character Select"
+    });
+  }
+
+  constructor(folderOptions) {
+    super();
+    this.folderOptions = folderOptions;
+  }
+
+  get templateData() {
+    return {
+      folderOptions: this.folderOptions
+    };
+  }
+
+  async _updateObject(event, formData) {
+    const folderId = formData.folder;
+    const folder = game.actors.get(folderId);
+    const actorIds = folder.content.map(actorId => folder.getEmbeddedEntity("Actor", actorId)._id);
+
+    for (const actorId of actorIds) {
+      const actor = game.actors.get(actorId);
+      const newActorData = duplicate(actor.data);
+      newActorData._id = null;
+      newActorData.permission.default = 2;
+      const newActor = await Actor.create(newActorData);
+      newActor.update({
+        "folder": folderId
+      });
+      ui.notifications.notify(`${newActor.name} created and permissions granted.`);
+    }
+  }
+}
+
+Hooks.on("canvasReady", () => {
+  const characterSelectTile = new CharacterSelectTile({
+    author: game.user._id,
+    x: 100,
+    y: 100,
+    width: CharacterSelectTile.dimensions.width,
+    height: CharacterSelectTile.dimensions.height,
+    fillAlpha: CharacterSelectTile.colors.alpha,
+    fillColor: CharacterSelectTile.colors.fill,
+    strokeAlpha: CharacterSelectTile.colors.alpha,
+    strokeColor: CharacterSelectTile.colors.stroke,
+    rotation: 0,
+    hidden: false,
+    locked: false,
+    z: 100,
+    flags: {},
+    texture: ""
+  });
+  canvas.drawingsLayer.addChild(characterSelectTile);
+});
+
+// Register the module
+Hooks.once("init", () => {
+  game.settings.register("unite-and-fight", "initialized", {
+    name: "Unite and Fight module initialized",
+    scope: "world",
+    config: false,
+    type: Boolean,
+    default: false
+  });
+
+  if (!game.settings.get("unite-and-fight", "initialized")) {
+    game.settings.set("unite-and-fight", "initialized", true);
+    ui.notifications.notify("Unite and Fight module initialized.");
+  }
+
+  game.packs.add("unite-and-fight.actors");
+});
+
+// Load templates
+Hooks.once("ready", async () => {
+  const templatePaths = [
+    "modules/unite-and-fight/templates/character-select-form.html"
+  ];
+
+  const
